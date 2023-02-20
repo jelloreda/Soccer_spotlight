07f378ca8637ee8d82e0efd5884c23aed07e0f95
jelloreda <jaimelloredavelasco@gmail.com>
2023-02-20 15:19:40 +0100

    conflict

--- middleware/local-session.js
+++ middleware/local-session.js
@@ -1,10 +1,12 @@
 const localSession = (req, res, next) => {
+
     if (req.session.currentUser) {
         res.locals.currentUser = req.session.currentUser
     } else {
         res.locals.currentUser = null
     }
     next()
+
 }
 
 module.exports = { localSession }
